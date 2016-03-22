c374f9699cb049cf9da42a43c92fe256f197536f
Shawn <sdgu000@gmail.com>
2016-03-22 00:53:07 -0400

    whole db in env var

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var mongoose = require("mongoose");
 var dbuser = process.env.USER;
 var dbpass = process.env.PASS;
 
-var dburi = "mongodb://" + dbuser + ":" + dbpass + "@ds061365.mongolab.com:61365/ineffablue";
+var dburi = process.env.DBURI;
 // var dburi = "mongodb://127.0.0.1:27017/ineffablue";
 var connextion = mongoose.connect(dburi);
 var db = connextion.connection;
